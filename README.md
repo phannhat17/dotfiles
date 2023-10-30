e02d13ed90ff3e46422220e0129d6cb2d347deb7
phannhat17 <phandinhnhat1708@gmail.com>
2023-10-30 16:15:11 +0700

    cm

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Personal dotfiles for ubuntu
-
-The themes in this repository do not belong to me. They are the [Colloid gtk theme from vinceliuice](https://github.com/vinceliuice/Colloid-gtk-theme). I am satisfied with this version of the theme, so I have decided to keep it without installing from source.
-
-The `open_in_code.sh` script file is also not mine but from [here](https://raw.githubusercontent.com/cra0zy/code-nautilus/master/install.sh). I am keeping this file locally to avoid the original file from being deleted
+# Personal dotfiles for ubuntu server
--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Minimize on Click
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
-
-# download wallpaper
-wget https://cdn.discordapp.com/attachments/940990932814282806/1090254486385217587/wallpaper.png
-
-# 
 sudo apt update     
 sudo apt upgrade -y
 
@@ -15,37 +8,24 @@ sudo apt install git -y
 sudo apt install curl -y
 sudo apt install npm
 
-
-# install tweaks and extensions
-sudo apt install gnome-tweaks -y
-sudo apt install gnome-shell-extensions -y
-
 # install nala
 sudo apt install nala -y
 
 # install neofetch
 sudo apt install neofetch -y
 
-# open in code, install chrome
-./open_in_code.sh
-./install_chrome.sh
-./install_docker_desktop.sh
-
 # install zsh and oh my zsh
-sudo apt-get install zsh
+sudo apt-get install zsh -y
 chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+yes | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # mv 
-cp -r .themes ~/
 cp .bashrc ~/
 cp .zshrc ~/
 
-
-
-
-
+# run zsh
+zsh
